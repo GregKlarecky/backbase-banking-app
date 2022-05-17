00776d79a8424648408d4291466ef44d2d924587
grzegorz.klarecki <grzegorz.klarecki@gmail.com>
2022-05-18 00:57:59 +0200

    unit test for review component

--- src/app/modules/core/components/review/review.component.spec.ts
+++ src/app/modules/core/components/review/review.component.spec.ts
@@ -16,7 +16,7 @@ describe('ReviewComponent', () => {
     await TestBed.configureTestingModule({
       declarations: [
         ReviewComponent,
-        MockPipe(PeachtreeCurrencyPipe),
+        MockPipe(PeachtreeCurrencyPipe, value => `${value} EUR`),
         MockComponent(SubmitButtonComponent)
       ],
       providers: [mockProvider(NgbActiveModal)]
@@ -30,7 +30,47 @@ describe('ReviewComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should display transfer receiver account', () => {
+    const template: HTMLElement = fixture.nativeElement;
+    const toAccount = template.querySelectorAll('[unit-test=to-account]')[0];
+    expect(toAccount.textContent).toEqual(reviewOptionsMock.toAccount);
+  });
+
+  it('should display transfer amount', () => {
+    const template: HTMLElement = fixture.nativeElement;
+    const amount = template.querySelectorAll('[unit-test=amount]')[0];
+    expect(amount.textContent).toEqual(`${reviewOptionsMock.amount} EUR`);
+  });
+
+  it('should close modal on close button click', () => {
+    const template: HTMLElement = fixture.nativeElement;
+    const closeButton = template.querySelectorAll(
+      '[unit-test=close-button]'
+    )[0];
+    const activeModalMock = TestBed.inject(NgbActiveModal);
+    (<HTMLElement>closeButton).click();
+    expect(activeModalMock.close).toHaveBeenCalled();
+  });
+
+  it('should close modal on dismiss button click', () => {
+    const template: HTMLElement = fixture.nativeElement;
+    const dismissButton = template.querySelectorAll(
+      '[unit-test=dismiss-button]'
+    )[0];
+    const activeModalMock = TestBed.inject(NgbActiveModal);
+    (<HTMLElement>dismissButton).click();
+    expect(activeModalMock.dismiss).toHaveBeenCalled();
+  });
+
+  it('should call changeBalance and makeTransfer functions and close the modal', () => {
+    const changeBalanceSpy = spyOn(component, 'changeBalance');
+    const makeTransferSpy = spyOn(component, 'makeTransfer');
+    const activeModalMock = TestBed.inject(NgbActiveModal);
+
+    component.onSubmitForm();
+
+    expect(changeBalanceSpy).toHaveBeenCalled();
+    expect(makeTransferSpy).toHaveBeenCalled();
+    expect(activeModalMock.close).toHaveBeenCalled();
   });
 });
