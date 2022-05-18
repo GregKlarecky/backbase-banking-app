6d8222b0299793601c6d621568d7ef93db128800
grzegorz.klarecki <grzegorz.klarecki@gmail.com>
2022-05-18 23:52:57 +0200

    cds on push

--- src/app/modules/transaction/components/amount/amount.component.ts
+++ src/app/modules/transaction/components/amount/amount.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { TransactionFacade } from '../../helpers/transaction.facade';
 
 @Component({
   selector: 'app-amount',
   templateUrl: './amount.component.html',
-  styleUrls: ['./amount.component.scss']
+  styleUrls: ['./amount.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AmountComponent {
   @Input() transaction: TransactionFacade;
